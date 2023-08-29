0eddd78764fb7296a2c8b63136063150aaddf888
Eric Krukowski <erickruk@amazon.com>
2023-08-29 01:54:19 -0400

    add game seeds --need to update relationships--

--- lib/db/models.py
+++ lib/db/models.py
@@ -20,12 +20,12 @@ class Game(Base):
     __tablename__ = 'game'
 
     id = Column('game_id', Integer(), primary_key=True)
-    user_name = Column('user_name', ForeignKey('users.user_name'))
+    user_username = Column('user_username', ForeignKey('users.user_username'))
     difficulty = Column('game_difficulty', String())
     outcome = Column('game_outcome', String())
 
     def __repr__(self):
         return f"<Game id={self.id}, " + \
-            f"user={self.user_name}, " + \
+            f"user={self.user_username}, " + \
             f"difficulty={self.difficulty}, " + \
             f"outcome={self.outcome}>"
--- lib/db/seed.py
+++ lib/db/seed.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import User, Game
 from faker import Faker
+import random
 
 fake = Faker()
 
@@ -16,6 +17,12 @@ user_list = []
 for i in range(10):
     user_list.append(User(username=fake.profile('username')['username'], name=fake['en-US'].name()))
     
-   
+diff_list = ['Easy', 'Medium', 'Hard']
+outcome_list = ['W', 'L']
+game_list = []
+for i in range(30):
+   game_list.append(Game(user_username=random.choice(user_list).username, difficulty=random.choice(diff_list), outcome=random.choice(outcome_list)))
+
 session.add_all(user_list)
+session.add_all(game_list)
 session.commit()
