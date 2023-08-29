5395b7ce2607c18dc003bb47b0f417d05cfc1c27
Eric Krukowski <erickruk@amazon.com>
2023-08-29 00:42:17 -0400

    add User class

---
+++ lib/db/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base 
+
+engine = create_engine('sqlite:///mine_data.db')
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column('user_id', Integer(), primary_key=True)
+    name = Column('user_name', String(50))
+    username = Column('user_username', String())
+
+    def __repr__(self):
+        return f"<User id={self.id}, " + \
+            f"name={self.name}, " + \
+            f"username = {self.username}>"
