ec62c74daa7d37170184ab819ad994da83da3d8e
Eric Krukowski <erickruk@amazon.com>
2023-08-29 20:34:56 -0400

    add print_board function

--- lib/helpers/__init__.py
+++ lib/helpers/__init__.py
@@ -1,1 +1,2 @@
 from .print import Print
+#from .new_game import NewGame
--- lib/helpers/print.py
+++ lib/helpers/print.py
@@ -1,18 +1,29 @@
 from art import *
-
+import os
 class Print:
     def welcome_message(self):
         tprint("minesweeper", font='block')
 
     def print_board(self, difficulty, board = {}):
     
-        size = 0
-        if (difficulty=='easy'):
-            size = 4
-        elif (difficulty=='medium'):
-            size = 7
-        elif (difficulty=='hard'):
-            size = 9
-
+        size = Print.setSize(difficulty)
+        os.system('clear')
+        print_line=" "
         for n in range(size):
-            pass
+            print_line+="  " + str(n)
+        print(print_line)
+        
+        for n in range(size):
+            print_line=str(n)
+            for m in range(size):
+                print_line+= "  "+ board[(n,m)]
+            print(print_line)
+
+    @classmethod      
+    def setSize(cls, difficulty):
+        if (difficulty=='easy'):
+            return 4
+        elif (difficulty=='medium'):
+            return 7
+        elif (difficulty=='hard'):
+            return 9
