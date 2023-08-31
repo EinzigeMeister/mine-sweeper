998a3f4e48e44d294e9d5db99299ec55b4c2398b
Eric Krukowski <erickruk@amazon.com>
2023-08-31 01:25:46 -0400

    Add Stats helper

--- lib/helpers/__init__.py
+++ lib/helpers/__init__.py
@@ -3,3 +3,4 @@ from .game_board import GameBoard
 from .choices import Choices
 from .session import session
 from .login import Login
+from .stats import Stats
---
+++ lib/helpers/stats.py
@@ -0,0 +1,5 @@
+from models import User, Game
+from helpers import session
+
+class Stats:
+    pass
