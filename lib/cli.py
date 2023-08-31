69f60ca23e830fb4c3a6c1439213418552c40622
Eric Krukowski <erickruk@amazon.com>
2023-08-31 00:07:36 -0400

    update file heirarchy

--- lib/cli.py
+++ lib/cli.py
@@ -1,18 +1,14 @@
-from helpers import Display, GameBoard, Choices
-
-    
+from helpers import Display, GameBoard, Choices, Login
 if __name__ == '__main__':
-  
 
     display = Display()
     display.welcome_message()
     user_logged_in = None
     while not user_logged_in:
-        user_selection = input("Please enter your username: ")
-        if len(user_selection)>0: user_logged_in = user_selection
+        user_logged_in = Login()
     menu_option_selected = 0
     while not menu_option_selected ==4:
-        menu_option_selected = Choices.main_menu(user_logged_in)
+        menu_option_selected = Choices.main_menu(user_logged_in.user)
         if menu_option_selected == 1:
             Choices.show_user_results()
         if menu_option_selected == 2:
--- lib/helpers/login.py
+++ lib/helpers/login.py
@@ -1,4 +1,19 @@
 from models import User
-
+from helpers import session
 class Login:
-   pass
+   def __init__(self):
+      self.user_login()
+
+   def user_login(self):
+      user_selection = input("Please enter your username: ")
+      if len(user_selection)>16: 
+         print("enter a username 16 characters or less")
+         self.user_login()
+      elif len(user_selection)==0:
+         print("No username detected, try again.")
+         self.user_login()
+      else:
+         self.user = user_selection
+      
+   def get_user(self):
+      return self.user
